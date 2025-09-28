c623111a70c6269899d710a260ae3f4e12286b66
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2025-09-28 18:28:19 +0600

    Update matrixMultiplication.cpp

--- Data Structure/matrixMultiplication.cpp
+++ Data Structure/matrixMultiplication.cpp
@@ -1,67 +1,33 @@
-
-ll MOD = 1e9 + 9;
-
-void matmul(vector<vector<ll>> &a, vector<vector<ll>> b) {
-	int n = a.size(), m = a[0].size(), p = b[0].size();
-	vector<vector<ll>> c(n, vector<ll>(p));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < p; j++) {
-			for (int k = 0; k < m; k++) {
-				c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % MOD;
-			}
-		}
-	}
-
-	a = c;
+vector<vector<ll>> matmul(const vector<vector<ll>> &A,
+                          const vector<vector<ll>> &B) {
+    vector<vector<ll>> C(3, vector<ll>(3, 0));
+    for (int i = 0; i < 3; i++)
+        for (int k = 0; k < 3; k++)
+            if (A[i][k])
+                for (int j = 0; j < 3; j++)
+                    C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+    return C;
 }
-
-struct Matrix {
-	vector<vector<ll>> mat;
-	Matrix() {}
-	Matrix(vector<vector<ll>> a) { mat = a; }
-	Matrix(int n, int m) {
-		mat.resize(n);
-		for (int i = 0; i < n; i++) { mat[i].resize(m); }
-	}
-
-	int rows() const { return mat.size(); }
-	int cols() const { return mat[0].size(); }
-
-	void makeiden() {
-		for (int i = 0; i < rows(); i++) { mat[i][i] = 1; }
-	}
-
-	Matrix operator*=(const Matrix &b) {
-		matmul(mat, b.mat);
-		return *this;
-	}
-
-	Matrix operator*(const Matrix &b) { return Matrix(*this) *= b; }
-};
-
-int main() {
-	ios::sync_with_stdio(false) , cin.tie(0);
-	ll n , m , c;
-	cin >> n >> m >> c;
-	Matrix mat(n, n), vec(n, 1), cur(n, n);
-	cur.makeiden();
-	for (int i = 0; i < n; i++) { cin >> vec.mat[i][0]; }
-
-	for (int i = 0; i < c; i++) {
-		int idx; cin >> idx;
-		debug(n - idx);	
-		mat.mat[n - 1][n - idx] = 1;
-	}
-
-	for (int i = 1; i < n; i++) { mat.mat[i - 1][i] = 1; }
-
-	m--;
-	while (m > 0) {
-		if (m & 1) cur *= mat;
-		mat *= mat;
-		m >>= 1;
-	}
-	Matrix res = cur * vec;
-	cout << res.mat[0][0] << '\n';
+ 
+vector<vector<ll>> matpow(vector<vector<ll>> n, ll k) {
+    vector<vector<ll>> ans(3, vector<ll>(3, 0));
+    for (int i = 0; i < 3; i++) ans[i][i] = 1;
+ 
+    while (k > 0) {
+        if (k & 1) ans = matmul(ans, n);
+        n = matmul(n, n);
+        k >>= 1;
+    }
+    return ans;
 }
-
+ 
+ll qp(ll a,ll b)
+{
+    ll x=1; a%=MOD;
+    while(b)
+    {
+        if(b&1) x=x*a%MOD;
+        a=a*a%MOD; b>>=1;
+    }
+    return x;
+}
